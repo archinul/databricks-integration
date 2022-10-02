e2a000b4d75e163fbb804a8027a99d41dad210d6
archinul <archinul>
2022-10-02 22:59:48 +0000

    Create iris model

---
+++ main.ipynb.py
@@ -0,0 +1,15 @@
+# Databricks notebook source
+# MAGIC %load_ext autoreload
+# MAGIC %autoreload 2
+
+# COMMAND ----------
+
+from helper import train_predict
+
+# COMMAND ----------
+
+train_predict()
+
+# COMMAND ----------
+
+
